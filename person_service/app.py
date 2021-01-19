3c63291b496478eacf35a0dbcdd5cc5fc1235d0e
Kim Nguyen <kimtnguyen4@gmail.com>
2021-01-19 00:03:45 -0800

    updates to APIs and tests

--- person_service/app.py
+++ person_service/app.py
@@ -20,6 +20,10 @@ config = cf.get_config()
 conn, cursor = db.make_db_connection(config=config)
 
 
+def clear_nones(d):
+    return {k: v for k, v in d.items() if v is not None}
+
+
 def create_app():
     app = Flask(__name__)
 
@@ -30,21 +34,19 @@ def create_app():
     @app.route('/person', methods=['POST'])
     def create_person():
         expected_list_of_args = ['first_name', 'last_name', 'age', 'email']
-        required_args = check_args(expected_list_of_args, request.json)
+
+        person_to_create = clear_nones(request.json)
+        required_args = check_args(expected_list_of_args, person_to_create)
         if required_args:
             return {'success': False,
                     'message': 'Missing fields',
                     'fields': required_args}, 400
-        person_to_create = request.json
         person = pf.create_person(cursor, person_to_create)
         conn.commit()
         return {'created': bool(person), 'person': person}, 200
 
     @app.route('/person/<person_id>', methods=['GET'])
     def get_person(person_id):
-        if not person_id:
-            return {'success': False,
-                    'message': 'Missing request argument person_id in path'}, 400
         person = pf.get_person_by_id(cursor, person_id)
         return {'success': True, 'person': person}, 200
 
@@ -74,20 +76,11 @@ def create_app():
                                  'age',
                                  'email']
 
-        if not person_id:
-            return {'success': False,
-                    'message': 'Missing required person_id'}, 400
-
         if not check_args(editable_list_of_args, request.json, at_least_one=True):
             return {'success': False,
                     'message': (f'Missing at least one of expected args '
                                 f'{editable_list_of_args}')}, 400
-        person_edits = request.json
-
-        for key in person_edits:
-            if person_edits[key] is None:
-                del person_edits[key]
-
+        person_edits = clear_nones(request.json)
         person = pf.get_person_by_id(cursor, person_id)
         person_copy = person.copy()
         person_copy.update(person_edits)
@@ -96,31 +89,21 @@ def create_app():
             return {'updated': False,
                     'person': person,
                     'message': 'No updated necessary with data passed in'}, 200
-        person_updated = pf.update_person(cursor, person)
+        person_updated = pf.update_person(cursor, person_copy)
         conn.commit()
         return {'updated': bool(person_updated), 'person': person_updated}, 200
 
-    @app.route('/person', methods=['DELETE'])
-    def delete_person():
-        expected_list_of_args = ['id']
-        required_args = check_args(expected_list_of_args, request.json)
-        if required_args:
-            return {'message': 'Missing fields',
-                    'fields': required_args}, 400
-
-        person_id = request.json['id']
+    @app.route('/person/<person_id>', methods=['DELETE'])
+    def delete_person(person_id):
+        person = pf.get_person_by_id(cursor, person_id)
+        if not person:
+            return {'message': 'No such person'}, 404
         pf.delete_person(cursor, person_id)
         conn.commit()
-        return {'deleted': True, 'id': person_id}, 200
+        return {'deleted': True, 'person_id': person_id}, 200
 
-    @app.route('/latest_person_version', methods=['DELETE'])
-    def delete_person_version():
-        expected_list_of_args = ['id']
-
-        if not check_args(expected_list_of_args, request.json):
-            return 'Missing id', 400
-
-        person_id = request.json['id']
+    @app.route('/latest_person_version/<person_id>', methods=['DELETE'])
+    def delete_person_version(person_id):
         person = pf.get_person_by_id(cursor, person_id)
         if not person:
             return {'message': 'No such person'}, 404
@@ -131,4 +114,4 @@ def create_app():
 
 
 if __name__ == '__main__':
-    print('derp')
+    print('Hello World!')
--- tests/test_app.py
+++ tests/test_app.py
@@ -18,7 +18,6 @@ def test_get_person(test_client):
     response = res.get_json()
     person = response['person']
     assert response['success']
-    assert person['age'] == 40
     assert person['first_name'] == 'Mary'
     assert person['middle_name'] == 'Eugene'
     assert person['last_name'] == 'Smith'
@@ -40,6 +39,20 @@ def test_create_person(test_client):
     assert person['person_id']
 
 
+def test_create_person_bad_request(test_client):
+    person_to_create = {
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'age': 25,
+        'email': None}
+    res = test_client.post('/person', json=person_to_create)
+    assert res.status_code == 400
+    response = res.get_json()
+    assert not response['success']
+    assert response['message'] == 'Missing fields'
+    assert response['fields'] == ['email']
+
+
 def test_update_person(test_client):
     person_id = 'ceb5770f-c4e3-4ad7-bc5e-4e32e8047e63'
     age = random.randint(0, 100)
@@ -48,4 +61,41 @@ def test_update_person(test_client):
                           json=person_to_update)
     response = res.get_json()
     person = response['person']
-    assert person['age'] == person_to_update['age']
+    assert person['age'] == age
+
+
+def test_get_person_by_version(test_client):
+    id = 'ceb5770f-c4e3-4ad7-bc5e-4e32e8047e63'
+    version = 4
+    res = test_client.get(f'/person_by_version/{id}?version={version}')
+    response = res.get_json()
+    person = response['person']
+    assert response['success']
+    assert person['first_name'] == 'Mary'
+    assert person['middle_name'] == 'Eugene'
+    assert person['last_name'] == 'Smith'
+    assert person['person_id'] == id
+    assert person['version'] == version
+
+
+def test_get_all_persons(test_client):
+    res = test_client.get('/person/all')
+    response = res.get_json()
+    people = response['people']
+    assert people
+
+
+def test_delete_person(test_client):
+    person_to_create = {
+        'first_name': fake.first_name(),
+        'last_name': fake.last_name(),
+        'age': 25,
+        'email': fake.email()}
+    res = test_client.post('/person', json=person_to_create)
+    response = res.get_json()
+    person = response['person']
+    person_id = person['person_id']
+
+    res = test_client.delete(f'/person/{person_id}')
+    response = res.get_json()
+    assert response['deleted']
