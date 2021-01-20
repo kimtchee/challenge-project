cc8727175cfac446538f1036a1f01422b987cc34
Kim Nguyen <kimtnguyen4@gmail.com>
2021-01-19 20:18:58 -0800

    Add script to initialize db with pg superuser

--- person_service/db_scripts/init_db.py
+++ person_service/db_scripts/init_db.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import psycopg2
 
 
 def init_db(conn):
@@ -21,3 +22,15 @@ def create_fixtures(conn):
     with conn.cursor() as cursor:
         cursor.execute(
             open(Path('person_service/db_scripts/deploy/0002_create_person_fixtures.sql'), 'r').read())
+
+
+if __name__ == '__main__':
+    from people_service.config import get_config
+    conn, cursor = db.make_db_connection(config=get_config())
+    try:
+        init_db(conn)
+        create_fixtures(conn)
+    except psycopg2.errors.lookup('42501') as e:
+        print(e)
+        print('Requires postgres superuser or create schema privilege')
+    print('Created db')
